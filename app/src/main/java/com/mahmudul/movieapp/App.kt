56949e105a6cd50c76f2356bf89aed6e7a814483
Hasan-Mac <hasan@klapify.com>
2022-09-08 23:33:18 +0600

    library install and UI setup

---
+++ app/src/main/java/com/mahmudul/movieapp/App.kt
@@ -0,0 +1,13 @@
+package com.mahmudul.movieapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
---
+++ app/src/main/java/com/mahmudul/movieapp/ui/movie/MovieFragment.kt
@@ -0,0 +1,24 @@
+package com.mahmudul.movieapp.ui.movie
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mahmudul.movieapp.R
+
+class MovieFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_movie, container, false)
+    }
+
+}
